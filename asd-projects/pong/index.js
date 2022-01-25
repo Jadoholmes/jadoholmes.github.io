e06988ddba4daef198bfc6709ad27fd3e5b887d0
Jadoholmes <holmesjadon437@gmail.com>
2022-01-25 14:06:31 +0000

    saving pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -10,7 +10,10 @@ function runProgram(){
   // Constant Variables
   const FRAME_RATE = 60;
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+  const BOARD_WIDTH = $("#board").width();
+  const BOARD_HEIGHT = $("#board").height();
+
+
   var KEY = {
     "UP": 38,
     "DOWN": 40,
@@ -36,7 +39,7 @@ return gameItem;
 var leftPaddle = factory("#leftPaddle");
 var rightPaddle = factory("#rightPaddle");
 var ball = factory("#ball");
-
+var board = factory("#board");
 
 
 
@@ -51,7 +54,7 @@ var ball = factory("#ball");
   $(document).on('keyup', handleKeyUp2); 
   startBall()
 
-
+//move right paddles
 function handleKeyDown(event) { 
   
    if (event.which === KEY.W) {
@@ -61,14 +64,20 @@ function handleKeyDown(event) {
     console.log("down pressed");
   }
 
- if (event.which === KEY.W){
+ if (event.which === KEY.W && wallColision(leftPaddle) === "truetop"){
+    leftPaddle.speedY = 0
+  }
+  else if (event.which === KEY.W  ){
     leftPaddle.speedY = -5
+  }
+  else if (event.which === KEY.S && wallColision(leftPaddle) === "truebottom"){
+    leftPaddle.speedY = 0
   }
   else if (event.which === KEY.S){
     leftPaddle.speedY = 5
   }
 }
-
+//sstop right paddles 
 function handleKeyUp(event) {
  if (event.which === KEY.W){
     leftPaddle.speedY = 0
@@ -78,7 +87,7 @@ function handleKeyUp(event) {
   }
   }
 
-
+//move left paddles
   function handleKeyDown2(event) { 
   
    if (event.which === KEY.UP) {
@@ -89,14 +98,22 @@ function handleKeyUp(event) {
       console.log("down pressed");
     }
   
-   if (event.which === KEY.UP){
-      rightPaddle.speedY = -5
-    }
-    else if (event.which === KEY.DOWN){
+     if (event.which === KEY.DOWN && wallColision(rightPaddle) === "truebottom"  ){
+      rightPaddle.speedY = 0
+      console.log(wallColision)
+  }
+    else if(event.which === KEY.DOWN ){
       rightPaddle.speedY = 5
     }
-  }
-  
+    else if (event.which === KEY.UP  && wallColision(rightPaddle) === "truetop"){
+      rightPaddle.speedY = 0
+    }
+    else if (event.which === KEY.UP ){
+      rightPaddle.speedY = -5
+    }
+   
+   
+  }//stop left paddles
   function handleKeyUp2(event) {
    if (event.which === KEY.UP){
       rightPaddle.speedY = 0
@@ -121,7 +138,7 @@ function handleKeyUp(event) {
   */
   function newFrame() {
     repositionGameItem()
-    redrawGameItem() //yes
+    moveObject() //yes
     
   }
   
@@ -137,26 +154,56 @@ function handleKeyUp(event) {
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
   
+function wallColision(obj){
+  board.leftX = board.x;
+  board.topY = board.y;
+  board.rightX = board.x + BOARD_WIDTH;
+  board.bottomY = board.y + BOARD_HEIGHT;
+
+ obj.leftX = obj.x;
+ obj.topY = obj.y;
+ obj.rightX = obj.x + obj.width;
+ obj.bottomY = obj.y + obj.height;
+
+
+
+  if(obj.topY <= board.topY 
+     ){
+      return "truetop"
+  }
+  else if(obj.bottomY >= board.bottomY){
+    return "truebottom"
+   
+  }
+}
+console.log(wallColision(rightPaddle))
 
   function repositionGameItem() {
     leftPaddle.x += leftPaddle.speedX
     leftPaddle.y += leftPaddle.speedY
     rightPaddle.x += rightPaddle.speedX
     rightPaddle.y += rightPaddle.speedY
-   
+    ball.x += ball.speedX
+    ball.y += ball.speedY
   }
 
-  function redrawGameItem() {
+  function moveObject() {
     $("#rightPaddle").css("top",rightPaddle.y);
     $("#leftPaddle").css("top", leftPaddle.y);
-
+    $(ball.id).css('left', ball.x);
+    $(ball.id).css('top', ball.y);
   }
  
   function startBall(){
     $('#ball')
     .css('top', 220)
-    .css('left', 220)
-
+    .css('left', 220);
+    randomNum = (Math.random() * 3+ 1) * (Math.random() > 0.5 ? -1 : 1);
+    randomNum2 = (Math.random() * 2 + 1) * (Math.random() > 0.5 ? -1 : 1);
+    ball.speedX = randomNum;
+    ball.speedY = randomNum2;
+   
+   
   }
 
   function endGame() {
